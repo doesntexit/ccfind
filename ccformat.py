21ce1804f35cb1aef1c60d1611beb3cf3b89eb62
Edwin <edwinhamal@gmail.com>
2021-08-21 08:17:36 -0700

    fixed nubie stuff

--- ccformat.py
+++ ccformat.py
@@ -1,7 +1,7 @@
 import re
 
 
-if __name__ == __main:
+if __name__ == "__main__":
     cc = input()
     print(formatcc(cc))
 
---
+++ main.py
@@ -0,0 +1,5 @@
+from ccformat import *
+
+if __name__ == "__main__":
+    cc = input("Enter CC to be formatted (in a single line) : ")
+    print(formatcc(cc))
